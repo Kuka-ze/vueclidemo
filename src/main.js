21738ada9a6699cb3a5bc6baa05f88d52ba034fb
shuaiZe <vuejs1024@163.com>
2020-05-22 22:41:57 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import '@/assets/css/reset.css'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.use(axios)
 
 new Vue({
     el: '#app',
